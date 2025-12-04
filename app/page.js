b7297ba04cfe128335896785f07c4451fd6f14ae
torikachi <a3601000er@gmail.com>
2025-12-04 18:05:24 +0700

    Updated

--- app/page.js
+++ app/page.js
@@ -1,45 +1,48 @@
-'use client';
+"use client";
 
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import React, { useState, useEffect } from 'react';
-import { Typography,
-         Box,
-         AppBar,
-         Toolbar,
-         Tab,
-         Tabs,
-         IconButton,
-         Drawer,
-         List,
-         ListItem,
-         ListItemText,
-         InputBase,
-         useMediaQuery,} from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-import ProductPage from './pages/product';
-import HomePage from './pages/home';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import React, { useState, useEffect } from "react";
+import {
+  Typography,
+  Box,
+  AppBar,
+  Toolbar,
+  Tab,
+  Tabs,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  InputBase,
+  useMediaQuery,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import ProductPage from "./pages/product";
+import HomePage from "./pages/home";
+import ContactPage from "./pages/contact";
 
 export default function App() {
   const theme = createTheme({
     palette: {
-      mode: 'light',
+      mode: "light",
       primary: {
-        main: '#1faa54ff',
-        light: '#37be3cff',
+        main: "#1faa54ff",
+        light: "#37be3cff",
       },
       secondary: {
-        main: '#ebff38ff',
+        main: "#ebff38ff",
       },
       text: {
-        primary: '#000000',
+        primary: "#000000",
       },
     },
     typography: {
-      fontFamily: 'Coiny, Roboto, Arial, sans-serif',
+      fontFamily: "Coiny, Roboto, Arial, sans-serif",
       h4: { fontWeight: 700 },
     },
     spacing: 8,
@@ -49,12 +52,12 @@ export default function App() {
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, [tab]);
-  
+
   const handleTabChange = (event, newValue) => {
-      setTab(newValue);
+    setTab(newValue);
   };
 
-  const isMobile = useMediaQuery(theme.breakpoints.down('md')); // màn hình nhỏ hơn md
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   const toggleDrawer = (open) => () => {
@@ -65,133 +68,232 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 16, padding: 16 }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 16,
+          padding: 16,
+        }}
+      >
         <CssBaseline />
-        <Box sx={{ height: '100%', width: '100%', borderRadius: 5, gap: 10, display: 'flex', alignItems: 'center', flexDirection: 'column', flexGrow: 1, marginTop: 15 }}>
-            {tab === 0 && <div style={{ overflowY: 'hidden', height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: 10 }}>
-              <HomePage/>
-            </div>}
-            {tab === 1 && <div style={{ overflowY: 'hidden', height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: 10 }}>
-              <ProductPage/>
-            </div>}
-            {tab === 2 && <div style={{ overflowY: 'hidden', height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: 10 }}>
-              <p style={{ color: '#1faa54ff', fontWeight: 'bold', fontSize: 50, marginTop: 5, textAlign: 'center' }}>Liên hệ </p>
-            </div>}
+        <Box
+          sx={{
+            height: "100%",
+            width: "100%",
+            borderRadius: 5,
+            gap: 10,
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            flexGrow: 1,
+            marginTop: 15,
+          }}
+        >
+          {tab === 0 && (
+            <div
+              style={{
+                overflowY: "hidden",
+                height: "100%",
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+                gap: 10,
+              }}
+            >
+              <HomePage />
+            </div>
+          )}
+          {tab === 1 && (
+            <div
+              style={{
+                overflowY: "hidden",
+                height: "100%",
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+                gap: 10,
+              }}
+            >
+              <ProductPage />
+            </div>
+          )}
+          {tab === 2 && (
+            <div
+              style={{
+                overflowY: "hidden",
+                height: "100%",
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+                gap: 10,
+              }}
+            >
+              <ContactPage />
+            </div>
+          )}
         </Box>
 
-        <div style={{ backdropFilter: 'blur(5px)', width: '100%', height: 100, display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'fixed', top: 0 }}>
-          <AppBar position='static' sx={{ borderRadius: 4, display: 'flex', justifyContent: 'center', width: '95%', backgroundColor: 'primary.main'}}>
-            {!openSearch ? (<Toolbar sx={{ p: 1, gap: 2 }}>
-              {isMobile ? (
-                <>
-                  <IconButton
-                    color="inherit"
-                    aria-label="menu"
-                    onClick={toggleDrawer(true)}
+        <div
+          style={{
+            backdropFilter: "blur(5px)",
+            width: "100%",
+            height: 100,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "fixed",
+            top: 0,
+          }}
+        >
+          <AppBar
+            position="static"
+            sx={{
+              borderRadius: 4,
+              display: "flex",
+              justifyContent: "center",
+              width: "95%",
+              backgroundColor: "primary.main",
+            }}
+          >
+            {!openSearch ? (
+              <Toolbar sx={{ p: 1, gap: 2 }}>
+                {isMobile ? (
+                  <>
+                    <IconButton
+                      color="inherit"
+                      aria-label="menu"
+                      onClick={toggleDrawer(true)}
+                    >
+                      <MenuIcon />
+                    </IconButton>
+                    <Drawer
+                      anchor="left"
+                      open={drawerOpen}
+                      onClose={toggleDrawer(false)}
+                    >
+                      <List sx={{ width: 250 }}>
+                        <ListItem
+                          onClick={() => handleTabChange(null, 0)}
+                          sx={{ cursor: "pointer" }}
+                        >
+                          <ListItemText primary="Trang chủ" />
+                        </ListItem>
+                        <ListItem
+                          onClick={() => handleTabChange(null, 1)}
+                          sx={{ cursor: "pointer" }}
+                        >
+                          <ListItemText primary="Sản phẩm" />
+                        </ListItem>
+                        <ListItem
+                          onClick={() => handleTabChange(null, 2)}
+                          sx={{ cursor: "pointer" }}
+                        >
+                          <ListItemText primary="Liên hệ" />
+                        </ListItem>
+                      </List>
+                    </Drawer>
+                  </>
+                ) : null}
+
+                <img
+                  src="https://github.com/soda2611/demo/blob/main/public/image.png?raw=true"
+                  alt="Logo"
+                  style={{ height: 40 }}
+                />
+                <Typography variant="h5" fontWeight="bold" component="div">
+                  GreenFarm
+                </Typography>
+
+                {!isMobile ? (
+                  <Tabs
+                    value={tab}
+                    onChange={handleTabChange}
+                    textColor="white"
+                    indicatorColor="secondary"
+                    variant="scrollable"
+                    scrollButtons="auto"
                   >
-                    <MenuIcon />
-                  </IconButton>
-                  <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
-                    <List sx={{ width: 250 }}>
-                      <ListItem onClick={() => handleTabChange(null, 0)} sx={{ cursor: 'pointer' }}>
-                        <ListItemText primary="Trang chủ" />
-                      </ListItem>
-                      <ListItem onClick={() => handleTabChange(null, 1)} sx={{ cursor: 'pointer' }}>
-                        <ListItemText primary="Sản phẩm" />
-                      </ListItem>
-                      <ListItem onClick={() => handleTabChange(null, 2)} sx={{ cursor: 'pointer' }}>
-                        <ListItemText primary="Liên hệ" />
-                      </ListItem>
-                    </List>
-                  </Drawer>
-                </>
-              ) : null}
+                    <Tab label="Trang chủ" />
+                    <Tab label="Sản phẩm" />
+                    <Tab label="Liên hệ" />
+                  </Tabs>
+                ) : null}
 
-              <img
-                src="https://github.com/soda2611/demo/blob/main/public/image.png?raw=true"
-                alt="Logo"
-                style={{ height: 40 }}
-              />
-              <Typography variant="h5" fontWeight="bold" component="div">
-                GreenFarm
-              </Typography>
+                <Box sx={{ flexGrow: 1 }} />
 
-              {!isMobile ? (
-                <Tabs
-                  value={tab}
-                  onChange={handleTabChange}
-                  textColor="white"
-                  indicatorColor="secondary"
-                  variant="scrollable"
-                  scrollButtons="auto"
-                >
-                  <Tab label="Trang chủ" />
-                  <Tab label="Sản phẩm" />
-                  <Tab label="Liên hệ" />
-                </Tabs>
-              ) : null}
-
-              <Box sx={{ flexGrow: 1 }} />
-
-              <IconButton
-                color="inherit"
-                aria-label="cart"
-                sx={{
-                  fontSize: 14,
-                  borderRadius: 2.5,
-                  backgroundColor: 'rgba(255, 255, 255, 0.3)',
-                }}
-              >
-                <ShoppingCartIcon />
-                {!isMobile && <Typography>Giỏ hàng</Typography>}
-              </IconButton>
-
-              {!isMobile ? (
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: 200,
-                    backgroundColor: 'white',
-                    borderRadius: 10,
-                    paddingLeft: 8,
+                <IconButton
+                  color="inherit"
+                  aria-label="cart"
+                  sx={{
+                    fontSize: 14,
+                    borderRadius: 2.5,
+                    backgroundColor: "rgba(255, 255, 255, 0.3)",
                   }}
                 >
-                  <SearchIcon sx={{ color: 'gray' }} />
-                  <input
-                    style={{
-                      height: 40,
-                      borderRadius: 10,
-                      outline: 'none',
-                      width: '100%',
-                      border: 'none',
-                      fontFamily: 'Coiny'
-                    }}
-                    placeholder="Tìm kiếm sản phẩm..."
-                  />
-                </div>
-              ) : (
-                <IconButton color="inherit" aria-label="search" onClick={() => setOpenSearch(true)}>
-                  <SearchIcon />
+                  <ShoppingCartIcon />
+                  {!isMobile && <Typography>Giỏ hàng</Typography>}
                 </IconButton>
-              )}
-            </Toolbar>) : (
-            <Toolbar sx={{ p: 1, gap: 2 }}>
-              <InputBase
-                autoFocus
-                placeholder="Nhập từ khóa..."
-                sx={{
-                  flex: 1,
-                  backgroundColor: "white",
-                  borderRadius: 2,
-                  paddingLeft: 2,
-                }}
-              />
-              <IconButton onClick={() => setOpenSearch(false)}>
-                <CloseIcon sx={{ color: "white" }} />
-              </IconButton>
-            </Toolbar>)}
+
+                {!isMobile ? (
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      width: 200,
+                      backgroundColor: "white",
+                      borderRadius: 10,
+                      paddingLeft: 8,
+                    }}
+                  >
+                    <SearchIcon sx={{ color: "gray" }} />
+                    <input
+                      style={{
+                        height: 40,
+                        borderRadius: 10,
+                        outline: "none",
+                        width: "100%",
+                        border: "none",
+                        fontFamily: "Coiny",
+                      }}
+                      placeholder="Tìm kiếm sản phẩm..."
+                    />
+                  </div>
+                ) : (
+                  <IconButton
+                    color="inherit"
+                    aria-label="search"
+                    onClick={() => setOpenSearch(true)}
+                  >
+                    <SearchIcon />
+                  </IconButton>
+                )}
+              </Toolbar>
+            ) : (
+              <Toolbar sx={{ p: 1, gap: 2 }}>
+                <InputBase
+                  autoFocus
+                  placeholder="Nhập từ khóa..."
+                  sx={{
+                    flex: 1,
+                    backgroundColor: "white",
+                    borderRadius: 2,
+                    paddingLeft: 2,
+                  }}
+                />
+                <IconButton onClick={() => setOpenSearch(false)}>
+                  <CloseIcon sx={{ color: "white" }} />
+                </IconButton>
+              </Toolbar>
+            )}
           </AppBar>
         </div>
       </div>
---
+++ app/pages/contact.js
@@ -0,0 +1,320 @@
+import React from "react";
+import { createTheme } from "@mui/material/styles";
+import {
+  Typography,
+  Box,
+  Button,
+  Grid,
+  useMediaQuery,
+  TextField,
+  Link,
+} from "@mui/material";
+
+export default function ContactPage() {
+  const theme = createTheme({
+    palette: {
+      mode: "light",
+      primary: {
+        main: "#1faa54ff",
+        light: "#37be3cff",
+      },
+      secondary: {
+        main: "#ebff38ff",
+      },
+      text: {
+        primary: "#000000",
+      },
+    },
+    typography: {
+      fontFamily: "Coiny, Roboto, Arial, sans-serif",
+      h4: { fontWeight: 700 },
+    },
+    spacing: 8,
+  });
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  return (
+    <div
+      style={{
+        width: "90%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 10,
+        backgroundColor: "primary.light",
+      }}
+    >
+      <Typography
+        variant="h3"
+        component="div"
+        sx={{ fontWeight: "bold", marginBottom: 10, color: "text.primary" }}
+      >
+        Liên hệ
+      </Typography>
+
+      {!isMobile ? (
+        <Grid
+          container
+          spacing={{ xs: 3, md: 3 }}
+          columns={{ xs: 2, sm: 4, md: 4 }}
+          sx={{ width: "70%", justifyContent: "center" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "initial",
+              flexGrow: 1,
+              gap: 25,
+            }}
+          >
+            <div style={{ display: "flex", gap: 25 }}>
+              <TextField
+                variant="outlined"
+                color="primary"
+                label="Họ và tên"
+                sx={{ flexGrow: 1 }}
+              />
+              <TextField
+                variant="outlined"
+                color="primary"
+                label="Số điện thoại"
+              />
+            </div>
+            <TextField variant="outlined" color="primary" label="Email" />
+            <TextField variant="outlined" color="primary" label="Chủ đề" />
+            <TextField
+              multiline
+              variant="outlined"
+              color="primary"
+              label="Nội dung"
+              rows={10}
+            />
+            <div>
+              <Button variant="contained" color="primary.main">
+                Gửi
+              </Button>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "self-start",
+              flexGrow: 1,
+            }}
+          >
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{
+                fontWeight: "bold",
+                marginBottom: 1,
+                color: "text.primary",
+              }}
+            >
+              GreenFarm
+            </Typography>
+            <Typography variant="body1">
+              Hotline: 0123456789
+              <br />
+              Email: example@mail.host
+              <br />
+              Địa chỉ: 123 đường ABC, phường XYZ, TPHCM
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                gap: 2,
+                alignItems: "center",
+                mt: 2,
+              }}
+            >
+              <Link>
+                <Box
+                  component="img"
+                  src="https://static.xx.fbcdn.net/rsrc.php/y1/r/ay1hV6OlegS.ico"
+                  sx={{
+                    width: 36,
+                    border: "3px solid #0866FF",
+                    borderRadius: "25px",
+                  }}
+                />
+              </Link>
+              <Link>
+                <Box
+                  component="img"
+                  src="https://static.cdninstagram.com/rsrc.php/v4/yI/r/VsNE-OHk_8a.png"
+                  sx={{
+                    border: "3px solid #fa00a4",
+                    borderRadius: "10px",
+                  }}
+                />
+              </Link>
+              <Link>
+                <Box
+                  component="img"
+                  src="https://www.tiktok.com/favicon.ico"
+                  sx={{
+                    border: "2px solid #24292e",
+                    borderRadius: "25px",
+                    bgcolor: "white",
+                  }}
+                />
+              </Link>
+            </Box>
+            <div
+              style={{
+                marginTop: 20,
+                display: "flex",
+                padding: 2,
+                gap: 10,
+                alignItems: "center",
+              }}
+            >
+              <img
+                src="https://github.com/soda2611/demo/blob/main/public/image.png?raw=true"
+                alt="Logo"
+                style={{ height: 40 }}
+              />
+              <Typography variant="h5" fontWeight="bold" component="div">
+                GreenFarm
+              </Typography>
+            </div>
+          </div>
+        </Grid>
+      ) : (
+        <Grid
+          container
+          spacing={{ xs: 3, md: 3 }}
+          columns={{ xs: 2, sm: 4, md: 4 }}
+          sx={{ width: "100%" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "initial",
+              flexGrow: 1,
+              gap: 25,
+            }}
+          >
+            <div style={{ display: "flex", gap: 25 }}>
+              <TextField
+                variant="outlined"
+                color="primary"
+                label="Họ và tên"
+                sx={{ flexGrow: 1 }}
+              />
+              <TextField
+                variant="outlined"
+                color="primary"
+                label="Số điện thoại"
+              />
+            </div>
+            <TextField variant="outlined" color="primary" label="Email" />
+            <TextField variant="outlined" color="primary" label="Chủ đề" />
+            <TextField
+              multiline
+              variant="outlined"
+              color="primary"
+              label="Nội dung"
+              rows={10}
+            />
+            <div>
+              <Button variant="contained" color="primary.main">
+                Gửi
+              </Button>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "self-start",
+              flexGrow: 1,
+            }}
+          >
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{
+                fontWeight: "bold",
+                marginBottom: 1,
+                color: "text.primary",
+              }}
+            >
+              GreenFarm
+            </Typography>
+            <Typography variant="body1">
+              Hotline: 0123456789
+              <br />
+              Email: example@mail.host
+              <br />
+              Địa chỉ: 123 đường ABC, phường XYZ, TPHCM
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                gap: 2,
+                alignItems: "center",
+                mt: 2,
+              }}
+            >
+              <Link>
+                <Box
+                  component="img"
+                  src="https://static.xx.fbcdn.net/rsrc.php/y1/r/ay1hV6OlegS.ico"
+                  sx={{
+                    width: 36,
+                    border: "3px solid #0866FF",
+                    borderRadius: "25px",
+                  }}
+                />
+              </Link>
+              <Link>
+                <Box
+                  component="img"
+                  src="https://static.cdninstagram.com/rsrc.php/v4/yI/r/VsNE-OHk_8a.png"
+                  sx={{
+                    border: "3px solid #fa00a4",
+                    borderRadius: "10px",
+                  }}
+                />
+              </Link>
+              <Link>
+                <Box
+                  component="img"
+                  src="https://www.tiktok.com/favicon.ico"
+                  sx={{
+                    border: "2px solid #24292e",
+                    borderRadius: "25px",
+                    bgcolor: "white",
+                  }}
+                />
+              </Link>
+            </Box>
+            <div
+              style={{
+                marginTop: 20,
+                display: "flex",
+                padding: 2,
+                gap: 10,
+                alignItems: "center",
+              }}
+            >
+              <img
+                src="https://github.com/soda2611/demo/blob/main/public/image.png?raw=true"
+                alt="Logo"
+                style={{ height: 40 }}
+              />
+              <Typography variant="h5" fontWeight="bold" component="div">
+                GreenFarm
+              </Typography>
+            </div>
+          </div>
+        </Grid>
+      )}
+    </div>
+  );
+}
