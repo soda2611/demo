d83068d705b28d8a52a5c13877eb9acb9f62bc3a
torikachi <a3601000er@gmail.com>
2025-12-05 12:41:52 +0700

    Updated

--- app/pages/home.js
+++ app/pages/home.js
@@ -194,6 +194,131 @@ export default function HomePage({ products }) {
           </>
         ))}
       </Box>
+      <Typography
+        variant="h3"
+        component="div"
+        sx={{
+          fontWeight: "bold",
+          marginTop: 10,
+          marginBottom: 1,
+          color: "text.primary",
+        }}
+      >
+        Đang giảm giá
+      </Typography>
+      <Box
+        sx={{
+          width: "100%",
+          mb: 15,
+          maxWidth: "1200px",
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, 250px)",
+          gap: 5,
+          justifyContent: "center",
+        }}
+      >
+        {Object.entries(products).map(([category, items]) => (
+          <>
+            {Object.entries(items).map(([name, item]) =>
+              item["sale"]!=item["price"] ? (
+                <Box
+                  sx={{
+                    backgroundColor: "#f1f1f1",
+                    width: 250,
+                    height: 400,
+                    borderRadius: 2.5,
+                  }}
+                >
+                  <div
+                    style={{
+                      backgroundColor: "#e0e0e0",
+                      width: "100%",
+                      height: "60%",
+                      overflow: 'hidden',
+                      borderTopLeftRadius: 10,
+                      borderTopRightRadius: 10,
+                    }}
+                  >
+                    <img
+                      src={item['img']}
+                      alt={name}
+                      style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                    />
+                  </div>
+                  <Box sx={{ padding: 2, height: "40%" }}>
+                    <Typography
+                      variant="h6"
+                      component="div"
+                      sx={{ fontWeight: "bold", marginBottom: 1 }}
+                    >
+                      {name}
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      color="text.secondary"
+                      sx={{ marginBottom: 2 }}
+                    >
+                      {`${item["description"]}`.length > 24
+                        ? `${item["description"]}`.substring(0, 22) + "..."
+                        : `${item["description"]}`}
+                    </Typography>
+                    <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                      <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ fontWeight: "bold", color: "primary.main" }}
+                      >
+                        {item["sale"]}₫
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        component="div"
+                        sx={{
+                          fontWeight: "bold",
+                          color: "gray",
+                          textDecoration: "line-through",
+                        }}
+                      >
+                        {item["price"]}₫
+                      </Typography>
+                    </Box>
+                    <IconButton
+                      sx={{ position: "relative", bottom: 13, left: 190 }}
+                      aria-label="favorite"
+                    >
+                      <ShoppingCartIcon />
+                    </IconButton>
+                    <IconButton
+                      sx={{ position: "relative", bottom: 360, left: -50 }}
+                      aria-label="favorite"
+                    >
+                      <FavoriteBorderIcon />
+                    </IconButton>
+                    <Box
+                      sx={{
+                        position: "relative",
+                        top: -395,
+                        left: 170,
+                        fontWeight: "bold",
+                        color: "white",
+                        backgroundColor: "primary.main",
+                        padding: 0.5,
+                        borderRadius: 2,
+                        fontSize: 12,
+                        textAlign: "center",
+                        width: 55,
+                      }}
+                    >
+                      -{(100 - (item["sale"] / item["price"]) * 100).toFixed(1)}
+                      %
+                    </Box>
+                  </Box>
+                </Box>
+              ) : null
+            )}
+          </>
+        ))}
+      </Box>
     </div>
   );
 }
